7dc2a3d05180087bff45b0fb73bbe976272a7cd3
shafeeq <shafeeqap1986@gmail.com>
2025-06-23 18:57:24 +0530

    server action

---
+++ my-app/src/app/server-action-user/page.js
@@ -0,0 +1,60 @@
+import { revalidatePath } from "next/cache";
+
+
+async function updateUser(formData) {
+  "use server"; // directive
+  const name = formData.get("name");
+
+  const response = await fetch(
+    "https://685945e5138a18086dfdcad9.mockapi.io/users",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name }),
+    }
+  );
+
+  revalidatePath("/users") // If you want the new user to appear after submission, you can call
+}
+
+export default async function ServerUsersAction() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  const response = await fetch(
+    "https://685945e5138a18086dfdcad9.mockapi.io/users"
+  );
+  const users = await response.json();
+
+  return (
+    <div>
+      <form action={updateUser} className="w-96 bg-slate-300 shadow-lg rounded-lg p-6 border border-gray-300 my-5">
+        <div className="flex-col items-center gap-4 mb-4">
+          <label className="text-gray-700 font-medium w-20">Name</label>
+          <input
+            name="name"
+            type="text"
+            placeholder="Enter your name"
+            className="flex p-2 border border-gray-300 rounded-lg focus:outline bg-white my-3"
+          />
+          <button
+            type="submit"
+            className="w-fit px-4 py-2 bg-blue-500 text-white rounded-md hover:cursor-pointer"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+      <h1>Users List (Server Component)</h1>
+      <ul>
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="bg-blue-100 border border-blue-300 p-2.5"
+          >
+            <strong className="text-blue-700">{user.name}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
